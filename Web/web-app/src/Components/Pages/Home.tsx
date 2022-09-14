88574714aa824ddd8c145259ac6c3059ba2d2e47
Armond Willingham <alw98@hotmail.com>
2022-09-14 17:20:03 -0500

    Still broken

--- Web/web-app/src/Components/Pages/Home.tsx
+++ Web/web-app/src/Components/Pages/Home.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import { createUseStyles } from 'react-jss';
 import Gearbox from 'wwwroot/images/GearAnimation.gif';
 
+import { ContentPageContainer } from './ContentPageContainer';
+
 export const HomePage: React.FC = () => {
 	const styles = useStyles();
 
 	return (
-		<img src={Gearbox} className={styles.gearboxImage} alt={'WIP'}/>
+		<ContentPageContainer>
+			<div className={styles.body}>
+				This is a gallery made by Armond Willingham. It's still a work in progress.
+			
+				<a href='https://github.com/alw98/awillingham-site'>Source</a>
+			</div>
+			<img src={Gearbox} className={styles.gearboxImage} alt={'WIP'}/>
+		</ContentPageContainer>
 	);
 };
 
 const useStyles = createUseStyles({
+	body: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		padding: '1rem 2rem 2rem 2rem',
+	},
 	gearboxImage: {
 		maxWidth: '100%',
 		maxHeight: '100%',
